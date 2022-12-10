17782f99b5fb63f95133c670908601e1fe70cc43
jinyeong <jinyeong>
2022-12-10 16:34:29 +0900

    이펙트 수정

---
+++ 1. Unity_File/Effect_Window_OTH/Assets/KUPAFX_S3/Post/PostCustom_2.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+[ExecuteInEditMode]
+public class PostCustom_2 : MonoBehaviour
+{
+    public Material material;
+    // Postprocess the image
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+    }
+}
